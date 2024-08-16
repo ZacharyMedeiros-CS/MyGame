dfbf310edc40bd2794e25f7ea243d8a9f0316685
Zachary Medeiros <zacharymedeiros_cs@outlook.com>
2024-08-16 15:03:36 -0700

    Playing around with canvas

---
+++ scripts/my_script.js
@@ -0,0 +1,17 @@
+
+
+
+console.log('Hello World');
+
+// To draw on the canvas, you'll need to access its rendering context.
+// The getContext("2d") method returns a 2D rendering context. This
+// context provides methods and properties for drawing shapes, text, 
+// and images on the canvas.
+
+/*
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "blue";
+context.fillRect(50, 50, 100, 100);
+*/
